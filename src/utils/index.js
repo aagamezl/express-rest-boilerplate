8c8f706ee4fb63579e9458ed23bad5a443c1e3dd
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2020-09-29 17:05:02 -0500

    fix: remove missing file import

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,14 +3,12 @@
  */
 
 const auth = require('./auth')
-const getRoutesMap = require('./getRoutesMap')
 const logger = require('./logger')
 const queries = require('./queries')
 const setRoutes = require('./setRoutes')
 
 module.exports = {
   ...auth,
-  getRoutesMap,
   logger,
   ...queries,
   setRoutes
